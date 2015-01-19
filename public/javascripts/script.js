771c3e511250cac521e9b7d6bc1ff7fec9b64e37
kensakukomatsu <kensaku.komatsu@gmail.com>
2015-01-19 12:56:30 +0900

    for issue #1 ボード一覧表示完了

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,14 +6,23 @@
 
 var onAuthorize = function(obj) {
   updateLoggedIn();
-  Trello.members.get("me", function(member){
+  Trello.members.get("me", { "boards": "all"}, function(member){
+    console.log(member);
     var mesg = [ "Welcome, ", member.fullName, " !!"].join("");
     $("#logged-in .welcome").text(mesg);
 
     var iconurl = [ "https://trello-avatars.s3.amazonaws.com/" , member.avatarHash , "/30.png" ].join("");
     $("<img>").attr("src", iconurl).appendTo("#logged-in .welcome");
-        
+
+    showBoards(member.boards);
+
   });
+}
+
+var showBoards = function(boards) {
+  console.log(boards);
+  boards.map(function(b) { b.url = "https://trello.com/b/" + b.id; });
+  $("#boards-container .boards ul").loadTemplate($("#board-template"), boards);
 }
 
 
