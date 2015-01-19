ad7cb46bbaa997154040b966f5c214b3216d238a
kensakukomatsu <kensaku.komatsu@gmail.com>
2015-01-19 17:12:55 +0900

    for issue #2 list,card,member iconを一覧表示するようになった

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -20,9 +20,67 @@ var onAuthorize = function(obj) {
 }
 
 var showBoards = function(boards) {
-  console.log(boards);
+  var $node = $("#boards-container .boards ul");
   boards.map(function(b) { b.url = "https://trello.com/b/" + b.id; });
-  $("#boards-container .boards ul").loadTemplate($("#board-template"), boards);
+  $node
+    .loadTemplate($("#board-template"), boards)
+    .find("input[type=checkbox]")
+    .on("click", getLists);
+}
+
+var getLists = function(){
+  var bid = $(this).data("bid");
+
+  Trello.boards.get(bid, {"cards": "all", "lists": "all", "members": "all"}, function(res) {
+    console.log(bid, res);
+    var cards = res.cards
+    , lists = res.lists
+    , members = res.members;
+
+    objMembers = {};
+    members.forEach(function(member) {
+      objMembers[member.id] = member;
+    });
+
+    // closedになっているlistは除外してから表示
+    lists = lists.filter(function(list) { return list.closed ? null : list });
+    $(".board[data-bid=" + bid + "] .lists ul").loadTemplate($("#list-template"), lists);
+
+    // listId毎にcardsを整形する
+    var objCards = {};
+    cards.forEach(function(card) {
+      var lid = card.idList;
+      if(!objCards[lid]) objCards[lid] = [];
+      card.mids = card.idMembers.join(",");
+      objCards[lid].push(card);
+    });
+    console.log(objCards);
+
+    // cardを表示する
+    for(var lid in objCards) if( objCards.hasOwnProperty(lid) ) {
+      var cards = objCards[lid];
+
+      $(".board[data-bid=" + bid + "] .list[data-lid=" + lid + "] .cards ul")
+        .loadTemplate($("#card-template"), cards);
+    }
+
+    // cardのassignメンバーを表示する（これだけ、template使うとつらそうなので、やめるぽ
+    $(".board[data-bid=" + bid + "] .members").each(function(ev) {
+      var mids = $(this).data("mids").split(",");
+      var $self = $(this);
+      mids.forEach(function(mid) {
+        if(mid && objMembers[mid]) {
+          var iconurl = [ "https://trello-avatars.s3.amazonaws.com/" , objMembers[mid].avatarHash , "/30.png" ].join("");
+          $("<img>").attr("src", iconurl).appendTo($self);
+        }
+      });
+    });
+  }, function(err) {
+    throw err;
+  });
+}
+
+var showLists = function(lists) {
 }
 
 
